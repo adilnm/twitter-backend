35ab1521380d20fb9379fed0679a471fab4125ec
Adil Naimi <adilnm23@gmail.com>
2020-04-08 23:21:56 -0500

    create index and create actions

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,22 @@
 class TweetsController < ApplicationController
+    def index
+        tweets=Tweet.all
+
+        render json:tweets
+    end
+end
+
+def create
+    tweet=Tweet.new(tweet_params)
+    if tweet.save
+        render json:tweet
+    else
+        render json:{error:'something is wrong'}
+    end
+end
+
+private
+
+def tweet_params
+    params.require(:tweet).permit(:body)
 end
