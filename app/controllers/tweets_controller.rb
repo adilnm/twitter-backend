35bec48fa7daa0ba6499abeb896616e6270eb29a
Adil Naimi <adilnm23@gmail.com>
2020-04-10 06:38:34 -0500

    fix the create action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,22 +1,21 @@
 class TweetsController < ApplicationController
     def index
         tweets=Tweet.all
-
         render json:tweets
     end
-end
 
-def create
-    tweet=Tweet.new(tweet_params)
-    if tweet.save
-        render json:tweet
-    else
-        render json:{error:'something is wrong'}
+    def create
+        tweet=Tweet.new(tweet_params)
+        if tweet.save
+            render json:tweet
+        else
+            render json:{error:'something is wrong'}
+        end
     end
-end
-
-private
-
-def tweet_params
-    params.require(:tweet).permit(:body)
+    
+    private
+    
+    def tweet_params
+        params.require(:tweet).permit(:body)
+    end
 end
